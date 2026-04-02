c1641e38625169a804c7568760c7af3b76bd0eb4
Ryan <rynthybyan0515gmail.com>
2026-04-02 18:06:15 -0400

    Agregue codigo a el controller del asistente

--- VitalConnect_API/Controllers/AsistenteController.cs
+++ VitalConnect_API/Controllers/AsistenteController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VitalConnect_API.Data;
+using VitalConnect_API.Models;
 
 namespace VitalConnect_API.Controllers
 {
@@ -7,6 +10,43 @@ namespace VitalConnect_API.Controllers
     [ApiController]
     public class AsistenteController : ControllerBase
     {
+        private readonly VT_DbContext _context;
 
+        public AsistenteController(VT_DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Asistente>>> GetAsistentes()
+        {
+            //Retornar asistentes
+            return Ok(await _context.Asistentes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Asistente>> GetAsistente(int id)
+        {
+            var asistente = await _context.Asistentes.FindAsync(id);
+
+            if (asistente == null) return NotFound();
+
+            return Ok(asistente);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsistente(Asistente asistente)
+        {
+            if (string.IsNullOrWhiteSpace(asistente.NombreCompleto)) return BadRequest("Debe Ingresar el Nombre Completo correctamente");
+
+            if(string.IsNullOrWhiteSpace(asistente.Telefono)) return BadRequest("Debe Ingresar el Telefono correctamente");
+
+            if (string.IsNullOrWhiteSpace(asistente.CI)) return BadRequest("Debe Ingresar el CI correctamente");
+
+            if(string.IsNullOrWhiteSpace(asistente.Rol)) return BadRequest("Debe Ingresar el Rol correctamente");
+
+            if (string.IsNullOrWhiteSpace(asistente.Turno)) return BadRequest("Debe Ingresar el Turno correctamente");
+            return Ok();
+        }
     }
 }
