de8dca4e5f6d827fc15e8c13e607866526868345
Ryan <rynthybyan0515gmail.com>
2026-04-02 20:17:00 -0400

    Agregue el Scalar a program para que se pueda usar y asi probar los controladores

--- VitalConnect_API/Program.cs
+++ VitalConnect_API/Program.cs
@@ -1,15 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using VitalConnect_API.Data;
+using Scalar.AspNetCore; //libreria de escalar
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<VT_DbContext>(options =>
     options.UseSqlite("Data Source=VitalConnect.db"));
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -18,6 +18,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    //Para probar en scalar
+    app.MapScalarApiReference();
 }
 
 app.UseAuthorization();
