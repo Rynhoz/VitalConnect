0b82d949ca40e35ae801d443f16770ab7b8cd2a5
Lucero <luceromarqueziphone@gmail.com>
2026-04-04 16:50:51 -0400

    Sugerencia de modificacion en RegistroPaciente para conectar con la ApiService

--- WEB_VitalConnectApi/Pages/Paciente/RegistroPaciente.cshtml.cs
+++ WEB_VitalConnectApi/Pages/Paciente/RegistroPaciente.cshtml.cs
@@ -1,10 +1,57 @@
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+
+//namespace WEB_VitalConnectApi.Pages
+//{
+//    public class RegistroPacienteModel : PageModel
+//    {
+//        [BindProperty]
+//        public string NombreCompleto { get; set; }
+
+//        [BindProperty]
+//        public string CI { get; set; }
+
+//        [BindProperty]
+//        public string Telefono { get; set; }
+
+//        [BindProperty]
+//        public string Genero { get; set; }
+
+//        [BindProperty]
+//        public string Direccion { get; set; }
+
+//        [BindProperty]
+//        public DateTime FechaNacimiento { get; set; }
+
+//        public IActionResult OnPost()
+//        {
+//            TempData["NombreCompleto"] = NombreCompleto;
+//            TempData["CI"] = CI;
+//            TempData["Telefono"] = Telefono;
+//            TempData["Genero"] = Genero;
+//            TempData["Direccion"] = Direccion;
+//            TempData["FechaNacimiento"] = FechaNacimiento.ToShortDateString();
+
+//            return RedirectToPage("/Paciente/Paciente");
+//        }
+//    }
+//}
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WEB_VitalConnectApi.Services;
 
 namespace WEB_VitalConnectApi.Pages
 {
     public class RegistroPacienteModel : PageModel
     {
+        private readonly ApiService _apiService;
+
+        public RegistroPacienteModel(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
         [BindProperty]
         public string NombreCompleto { get; set; }
 
@@ -23,16 +70,33 @@ namespace WEB_VitalConnectApi.Pages
         [BindProperty]
         public DateTime FechaNacimiento { get; set; }
 
-        public IActionResult OnPost()
-        {
-            TempData["NombreCompleto"] = NombreCompleto;
-            TempData["CI"] = CI;
-            TempData["Telefono"] = Telefono;
-            TempData["Genero"] = Genero;
-            TempData["Direccion"] = Direccion;
-            TempData["FechaNacimiento"] = FechaNacimiento.ToShortDateString();
+        public string Mensaje { get; set; }
 
-            return RedirectToPage("/Paciente/Paciente");
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var paciente = new
+            {
+                NombreCompleto = NombreCompleto,
+                CI = CI,
+                Telefono = Telefono,
+                Genero = Genero,
+                Direccion = Direccion,
+                FechaNacimiento = FechaNacimiento
+            };
+
+            var guardado = await _apiService.PostAsync("api/Paciente", paciente);
+
+            if (guardado)
+            {
+                return RedirectToPage("/Paciente/Paciente");
+            }
+
+            Mensaje = "No se pudo registrar el paciente";
+            return Page();
         }
     }
 }
